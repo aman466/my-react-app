03008673d84153c3769ba45ae41baa3e2f80f042
Aman Kumar <aman.kumar466@gmail.com>
2026-01-28 15:35:36 +0530

    Notes App: Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import CreateArea from "./CreateArea";
+import Note from "./Note";
+import Header from "./Header";
+import Footer from "./Footer";
+
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((note, index) => index !== id);
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((note, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={note.title}
+            content={note.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
